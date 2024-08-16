ebf910c4dfa28f5305c533544ef779688f2d9acb
Subham Sahu <about.subhamsahu@gmail.com>
2024-08-16 21:50:16 +0530

    .

--- provider/StoreProvider.tsx
+++ provider/StoreProvider.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { makeStore } from "@/lib/store/store";
-import React, { ReactNode } from "react";
+import { useRef } from "react";
 import { Provider } from "react-redux";
+import { makeStore, AppStore } from "@/lib/store/store";
 
-const StoreProvider = ({ children }: { children: ReactNode }) => {
-  return <Provider store={makeStore()}>{children}</Provider>;
+const StoreProvider = ({ children }: { children: React.ReactNode }) => {
+  const storeRef = useRef<AppStore>();
+
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
 };
 
 export default StoreProvider;
